978f29e022891eac7f23bc96e299f04b9e6f6217
durranee <durranee@hotmail.com>
2018-02-21 12:49:54 +0000

    unit test one-liner syntax done

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require './lib/docking_station'
 describe DockingStation do
-    it "responde to release_bike" do
-      expect(subject).to respond_to (:release_bike)
-    end
+#    it "responde to release_bike" do
+#      expect(subject).to respond_to (:release_bike)
+#    end
+it { should respond_to(:release_bike) }
 end
