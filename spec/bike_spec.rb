75c915943b29ec96b6fc6608a46679a604c7bbd5
Gia1987 <giacomo.n27@gmail.com>
2018-02-21 14:41:25 +0000

    Making stations release bikes and bike working? test passed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,6 @@ require './lib/bike'
 
 describe Bike do
   it { should respond_to(:is_working?) }
+
+  it {expect(subject.is_working?).to be(true)}
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,6 @@ describe DockingStation do
 #      expect(subject).to respond_to (:release_bike)
 #    end
 it { should respond_to(:release_bike) }
+
+it { expect(subject.release_bike).to be_an(Bike) }
 end
